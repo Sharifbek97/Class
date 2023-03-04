87eb92e5d926f0f985f0f33a49516115870a8136
Sharifbek97 <sharifbekmirzozoda2@gmail.com>
2023-03-04 15:55:29 +0500

    Added Cycle class with more method

---
+++ Class/Cycle.cs
@@ -0,0 +1,15 @@
+namespace Class;
+
+public class Cycle
+{
+    public double Area(double PI, double R)
+    {
+        return (PI * R )* R;
+    }
+
+    public double Perimeter(double PI, double R)
+    {
+        return 2 * PI * R;
+    }
+}
+
--- Class/Program.cs
+++ Class/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Class;
 
-var person = new Person();
+
+//Project 1
+/*var person = new Person();
 
 person.Name = "Sharifbek";
 person.LastName = "Mirzozoda";
@@ -10,13 +12,25 @@ person.BirthDay = Convert.ToDateTime("12-20-1973");
 person.PrintFullNameAndAge(person.GetFullName(),person.GetAge());
 
 Console.WriteLine($"ShortName: {person.GetShortName()} " +
-                  $"BirthDay: {person.BirthDay}   Age: {person.GetAge()}");
+                  $"BirthDay: {person.BirthDay}   Age: {person.GetAge()}");*/
 
-
-var colculater = new Colculater();
+//Project 2
+/*var colculater = new Colculater();
 Console.WriteLine("Number 1: ");
 double A = double.Parse(Console.ReadLine()); 
 Console.WriteLine("Number 2: ");
 double B = double.Parse(Console.ReadLine()); 
         Console.WriteLine($"\n\tSumma: {colculater.Summa (A,B)} \n\tDifference: {colculater.Difference(A,B)}"+
-                          $"\n\tMultiply: {colculater.Multiply(A,B)}  \n\tDivision: {colculater.Division(A,B)}");
+                          $"\n\tMultiply: {colculater.Multiply(A,B)}  \n\tDivision: {colculater.Division(A,B)}");*/
+                          
+                          
+//Project 3
+var cycle = new Cycle();
+
+Console.Write("Radius of cycle: R = ");
+double R = double.Parse(Console.ReadLine()); 
+double PI=3.14;
+Console.WriteLine($"\n\tArea of cycle: {cycle.Area(PI,R)}  \n\tPerimeter of cycle: {cycle.Perimeter(PI,R)}");
+
+                          
+                          
