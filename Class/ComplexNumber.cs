f6b92cc94dd2617929df0be477373f4aee5f4745
Sharifbek97 <sharifbekmirzozoda2@gmail.com>
2023-03-10 16:32:47 +0500

    Added class ComplexNumber and more method

---
+++ Class/ComplexNumber.cs
@@ -0,0 +1,14 @@
+namespace Class;
+
+
+public class ComplexNumber
+{
+    public int GetReal(int a)
+    {
+        return a;
+    }
+    public int GetImaginary(int b)  //Kismi mavhumi adadi kompleksi
+    {
+        return b;
+    }
+}
